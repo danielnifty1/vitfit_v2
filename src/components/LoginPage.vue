1e87941a53fa4c531cdd92fe4d3bbdc7988e5694
danielnifty <danielobichere@gmail.com>
2024-07-03 12:32:21 +0100

    fix

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+</script>
+<template>
+
+    <div class="h-[500px]">
+<h1 class="text-white text-center font-poppins font-bold text-3xl">Login PAGE</h1>
+    </div>
+</template>
--- src/core/router/router.ts
+++ src/core/router/router.ts
@@ -7,12 +7,16 @@ const HomeView=()=>import( "../../components/HomeView.vue");
 const programslide=()=>import( "../../components/programslide.vue");
 const testimonialview=()=>import( "../../components/testimonialView.vue");
 const FooterFile=()=>import("../../components/FooterFile.vue");
-// const contactpage=()=>import( "../../components/ContactPage.vue");
+const ServicePage=()=>import("../../components/ServicePage.vue");
+const Programpage=()=>import( "../../components/ProgramPage.vue");
+
+
+const contactpage=()=>import( "../../components/ContactPage.vue");
 // const buttons=()=>import("../../components/Buttons.vue");
 // const BreadCrumb=()=>import("../../components/BreadCrumb.vue");
 
 // const Regform=()=>import("../../components/Regform.vue");
-// const LoginPage=()=>import("../../components/LoginPage.vue");
+const LoginPage=()=>import("../../components/LoginPage.vue");
 // const OtpPage=()=>import("../../components/OtpPage.vue");
 
 
@@ -20,13 +24,15 @@ const routes: Array<RouteRecordRaw> = [
   { path: "/", redirect: "/home" },
 //   { path: "/home", name: "page.home", components: {default:HomeView, NavBar: NavBar,programslide:programslide,FooterPage: FooterFile } },
   { path: "/home", name: "page.home", components: {default:HomeView, NavBar: NavBar,programslide:programslide,testimonialview:testimonialview,FooterFile:FooterFile}, },
-//   { path: "/book-appointment", name: "Appointment-Booking", components: {default:aboutpage, NavBar: HeaderFile,banner: BreadCrumb,FooterPage: FooterFile } },
+  { path: "/services", name: "services", components: {default:ServicePage, NavBar: NavBar,FooterFile: FooterFile } },
+  { path: "/programs", name: "programs", components: {default:Programpage, NavBar: NavBar,FooterFile: FooterFile } },
+
 //   { path: "/services",name:"Programs", components: { default: Servicespage, NavBar: HeaderFile, banner: BreadCrumb, FooterPage: FooterFile, } },
-//   { path: "/contact",name:"Contact-Us", components: { default: contactpage,NavBar: HeaderFile, banner: BreadCrumb, FooterPage: FooterFile, } },
+   { path: "/contact",name:"Contact", components: { default: contactpage,NavBar: NavBar, FooterFile: FooterFile, } },
 //   { path: "/signup", components: { default: Regform, NavBar: HeaderFile, FooterPage: FooterFile,buttons:buttons } },
 //    { path: "/verify-email",name:"verify", components: { default: OtpPage, NavBar: HeaderFile, FooterPage: FooterFile, } },
 
-//   { path: "/login", components: {default:LoginPage,NavBar:HeaderFile,FooterPage:FooterFile  }},
+  { path: "/login", components: {default:LoginPage,NavBar:NavBar,FooterFile:FooterFile  }},
 //   { path: "/signup", components: { default: NewAccount, banner: banner, FooterPage: FooterPage } },
 //   { path: "/register", components: { default: RegisterPage, banner: banner, FooterPage: FooterPage, } },
 
