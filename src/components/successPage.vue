18ec3647ab92b44db63022e1b05b730a8f5d6a82
danielnifty <danielobichere@gmail.com>
2024-08-22 15:18:27 +0100

    adding mail logo

--- src/components/successPage.vue
+++ src/components/successPage.vue
@@ -4,11 +4,11 @@ const proceed = () => {
 }
 </script>
 <template>
-  <div class="grid grid-cols-1 md:pl-24 md:pr-24 place-items-center">
-    <div  class="bg-[#FAF6FD0D] grid grid-cols-1 w-6/12 place-items-center p-10 space-y-5">
+  <div class="grid grid-cols-1 md:pl-24 md:pr-24 place-items-center h-full ">
+    <div  class="bg-[#FAF6FD0D] grid grid-cols-1 md:w-6/12 place-items-center md:p-10 p-10 mb-5 mt-5 space-y-5">
         <img src="/images/Group.png" />
 
-        <h2 class="text-4xl text-[#2598F1] font-bold">BOOKING SUCCESSFUL!!</h2>
+        <h2 class="md:text-4xl text-[#2598F1] font-bold text-center">BOOKING SUCCESSFUL!!</h2>
 
         <p class="text-center text-[#FAF6FD80]">Thank for Arranging Your Free Consultation Call with VITFITYou will soon receive a text and email confirmation to inform you that we have successfully received your application.</p>
 
